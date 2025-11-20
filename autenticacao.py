cd57144f8d13166b420816f4862eaf676cf70874
vinicius.aoli <vinicinhouz7@gmail.comgit config --global user.email vinicinhouz7@gmail.com>
2025-11-19 23:08:47 -0300

    Commit2

---
+++ autenticacao.py
@@ -0,0 +1,19 @@
+def fazer_login(db_usuarios, login, senha):
+    """
+    Verifica se um login e senha são válidos no nosso banco de dados
+    Caso contrário, retorna Inválido
+    """
+    
+    usuario = db_usuarios.get(login)
+
+    if not usuario:
+        return "Inválido", None
+    
+    if usuario['senha'] == senha:
+        perfil = usuario['perfil']
+        id_aluno = usuario['id_aluno']
+        return perfil, id_aluno
+    
+    else: 
+        return "Invalido", None
+    
--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import gerenciador_dados
+import autenticacao
 
 def main():
     print("Iniciando o sistema...")
     db_usuarios = gerenciador_dados.carregar_usuarios('usuarios.csv')
     db_perfis = gerenciador_dados.carregar_perfis('perfis_alunos.csv')
     
-    #TESTES (apagar depois)
-    print("Dados do admin:", db_usuarios.get('admin'))
-    print("Dados do aluno1:", db_usuarios.get('aluno1'))
+    if not db_usuarios or not db_perfis:
+        print("Falha ao carregar arquivos de dados.")
+        return
+    
+    while True:
+        print("\n Bem-vindo ao GymStat!")
+        login = input("Digite seu login (ou 'sair' para encerrar): ")
+        if login.lower() == 'sair':
+            break
 
-    print("Perfil do A001:", db_perfis.get('A001'))
+        senha = input("Digite a sua senha: ")
+        perfil, id_aluno = autenticacao.fazer_login(db_usuarios, login, senha)
+
+        if perfil == "Gerente":
+            print(f"\nBem-vindo, gerente {login}!")
+            break
+
+        elif perfil == "Aluno":
+            print(f"\nBem-vindo, aluno {login}!")
+            break
+
+        else:
+            print("\n Erro: login ou senha inválidos. Tente novamente.")
+
+    print("Obrigado por usar o GymStat!")
 
 if __name__ == "__main__":
     main()
